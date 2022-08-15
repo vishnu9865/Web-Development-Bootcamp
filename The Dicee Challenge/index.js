2008810142a5751c8f8418a4a8e07e6d9e320462
Jeyavishnu S <sjeyavishnu9865@gmail.com>
2022-08-15 13:46:46 +0530

    Drum Kit

--- The Dicee Challenge/index.js
+++ The Dicee Challenge/index.js
@@ -1,15 +1,22 @@
-window.onbeforeunload = () => {
-    
-    let random1 = 0, random2 = 0;
 
-    do {
-        random1 = Math.round(Math.random()*6) + 1;
-        random2 = Math.round(Math.random()*6) + 1;
-    } while( random1 === random2);
+let random1 = 0, random2 = 0;
 
-    var player1 = document.querySelector(".img1");
-    var player2 = document.querySelector(".img2");
-    player1.setAttribute('src','images/dice'+random1+'.png');
-    player2.setAttribute('src','images/dice'+random2+'.png');
+random1 = Math.floor(Math.random()*6) + 1;
+random2 = Math.floor(Math.random()*6) + 1;
 
+var player1 = document.querySelector(".img1");
+player1.setAttribute('src','images/dice'+random1+'.png');
+
+var player2 = document.querySelector(".img2");
+player2.setAttribute('src','images/dice'+random2+'.png');
+
+var heading = document.querySelector( '.container h1');
+
+if( random1 === random2) {
+    heading.innerHTML = '🚩Draw🚩';
+} else if ( random1 > random2) {
+    heading.innerHTML = '🚩Player 1 Wins';
+} else {
+    heading.innerHTML = 'Player 2 Wins🚩';
 }
+
