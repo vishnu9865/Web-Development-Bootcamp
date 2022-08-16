559e192584c0a79cdddf994a9c292759d3657022
Jeyavishnu S <sjeyavishnu9865@gmail.com>
2022-08-16 20:12:35 +0530

    more edits.

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,1 +1,11 @@
-$("h1").css("color", "red");
+$('h1').on('mouseover', () =>{
+    $('h1').css('color', 'purple');
+});
+
+$('h1').on('mouseleave', () =>{
+    $('h1').css('color', 'black');
+});
+
+$('button').click( () => {
+   $('h1').animate({opacity: 0.5});
+});
