0a96748450a82ec9407f754f2aef6c48b5d9e975
Jeyavishnu S <sjeyavishnu9865@gmail.com>
2022-08-16 06:56:38 +0530

    more edits

--- Drum Kit Challenge/index.js
+++ Drum Kit Challenge/index.js
@@ -4,8 +4,51 @@
 
 var btnarray = document.querySelectorAll( '.drum');
 
-for( let button of btnarray) {
-    button.addEventListener( 'click', () => {
-        alert( 'I just got Clicked Lol');
+for( let i = 0; i < btnarray.length; i ++) {
+    btnarray[i].addEventListener( 'click', function(){
+        makeSound( this.innerHTML);
+        btnAnimation( this.innerHTML);
     });
+}
+
+document.addEventListener( 'keydown', function( event) {
+    //document.querySelector( '.'+event.key).click();
+    makeSound( event.key);
+    btnAnimation( event.key);
+});
+
+function makeSound( key) {
+    switch( key){
+        case 'w':
+            new Audio('sounds/tom-1.mp3').play();
+            break;
+        case 'a':
+            new Audio( 'sounds/tom-2.mp3').play();
+            break;
+        case 's':
+            new Audio( 'sounds/tom-3.mp3').play();
+            break;
+        case 'd':
+            new Audio( 'sounds/tom-4.mp3').play();
+            break;
+        case 'j':
+            new Audio( 'sounds/snare.mp3').play();
+            break;
+        case 'k':
+            new Audio( 'sounds/crash.mp3').play();
+            break;
+        case 'l':
+            new Audio( 'sounds/kick-bass.mp3').play();
+            break;
+        default:
+            break;
+    }
+}
+
+function btnAnimation( currentKey){
+    var activeButton = document.querySelector( '.'+currentKey);
+    activeButton.classList.add( 'pressed');
+    setTimeout( function() { 
+        activeButton.classList.remove( 'pressed');
+    }, 100)
 }
