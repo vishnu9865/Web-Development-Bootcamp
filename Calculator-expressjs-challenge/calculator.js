47b9354f7ebae6b44b27fd95e2b2ef50d3882fd2
Jeyavishnu S <sjeyavishnu9865@gmail.com>
2022-08-17 18:59:46 +0530

    more edits..

--- Calculator-expressjs-challenge/calculator.js
+++ Calculator-expressjs-challenge/calculator.js
@@ -1,13 +1,12 @@
 const express = require('express');
-
 const app = express();
-
 const port = 3000;
 
-app.listen( port, ( hostname) => {
-    console.log('Server ' + hostname + ' started at ' + port);
+//start listening to port
+app.listen( port, () => {
+    console.log('Server started at ' + port);
 });
 
 app.get('/', ( req, res) => {
-    res.send( '<h1>Hello World</h1>');
+    res.sendFile( __dirname+"/index.html");
 });
