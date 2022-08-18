48dc7a63b798d5d4505479a51288dff878573307
Jeyavishnu S <sjeyavishnu9865@gmail.com>
2022-08-18 22:09:33 +0530

    More edits..

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const https = require("https");
-const request = require("request");
+//const request = require("request");
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 app.listen( 3000, () => {
     console.log("Started server on port 3000");
 });
 
 app.get( "/", (req, res) => {
+    //res.sendFile( __dirname + "/public/styles.css");
     res.sendFile( __dirname + "/signup.html");
 });
