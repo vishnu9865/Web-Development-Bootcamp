5c1f65638beceff2081ee7ebdd6a95abb9f18726
Jeyavishnu S <sjeyavishnu9865@gmail.com>
2022-08-19 06:21:24 +0530

    more edits

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -48,13 +48,11 @@ app.post( "/signup", ( req, res) => {
 
     const request = https.request( url, options, ( response) => {
 
-        // if( response.statusCode === 200) {
-        //     res.sendFile( __dirname + "/success.html");
-        // } else {
-        //     res.sendFile( __dirname + "/failure.html");
-        // }
-
-        res.send(response.statusCode);
+        if( response.statusCode === 200) {
+            res.sendFile( __dirname + "/success.html");
+        } else {
+            res.sendFile( __dirname + "/failure.html");
+        }
 
         response.on( "data", ( data) => {
             console.log(JSON.parse(data));
