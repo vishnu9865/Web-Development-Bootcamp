b22a075164b1281a6b4ae549d6c43f964399fb8e
Jeyavishnu S <sjeyavishnu9865@gmail.com>
2022-08-20 14:01:07 +0530

    more edits..

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -9,23 +9,32 @@ app.use(bodyParser.urlencoded({extended:true}));
 // initialize and use ejs
 app.set( "view engine", "ejs");
 
+//set the static directory to load css and media from
+app.use(express.static('public'));
+
 // listen on port 3000
 app.listen( 3000, () => {
     console.log("Sever started on port 3000");
 });
+
 // reply to get '/'
 app.get("/", ( req, res) => {
+
     var today = new Date();
-    var day = "";
-    if( today.getDay() === 6 || today.getDay() === 0) {
-        day = "Weekend";
-        //res.sendFile(__dirname+'/index.html');
-    }else {
-        day = "Weekday";
-        //res.sendFile(__dirname+'/index.html');
-    }
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    var date = today.toLocaleDateString("en-IN", options);
 
     res.render( 'list', {
-        dayEJS: day
+        dayEJS: date
     });
+
+});
+
+app.post("/", (req, res) => {
+    console.log(req.body.newItem);
 });
