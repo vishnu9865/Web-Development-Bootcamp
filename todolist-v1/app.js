ae9e422a07b32c06994caf7e1ab3d208845f5ebc
Jeyavishnu S <sjeyavishnu9865@gmail.com>
2022-08-20 09:30:54 +0530

    create ejs file

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,9 +1,14 @@
 // initialize express
 const express = require("express");
 const app = express();
+
 // initialize and use express
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended:true}));
+
+// initialize and use ejs
+app.set( "view engine", "ejs");
+
 // listen on port 3000
 app.listen( 3000, () => {
     console.log("Sever started on port 3000");
@@ -11,8 +16,16 @@ app.listen( 3000, () => {
 // reply to get '/'
 app.get("/", ( req, res) => {
     var today = new Date();
-    if( today.getDay() === 6 || today.getDay() === 0)
-        res.send("It is the weekend")
-    else
-        res.send("Boo! I have to work!");
+    var day = "";
+    if( today.getDay() === 6 || today.getDay() === 0) {
+        day = "Weekend";
+        //res.sendFile(__dirname+'/index.html');
+    }else {
+        day = "Weekday";
+        //res.sendFile(__dirname+'/index.html');
+    }
+
+    res.render( 'list', {
+        dayEJS: day
+    });
 });
