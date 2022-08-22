94c4acecef98dfb812716c0c34502fed4e9e99e0
Jeyavishnu S <sjeyavishnu9865@gmail.com>
2022-08-23 05:18:09 +0530

    More edits..

--- Fruits-Project/app.js
+++ Fruits-Project/app.js
@@ -4,6 +4,9 @@ const assert = require('assert')
 
 const uri =
   "mongodb://localhost:27017";
+
+const dbname = 'fruitsDB';
+
 const client = new MongoClient(uri);
 
 client.connect(function(err) {
@@ -11,5 +14,31 @@ client.connect(function(err) {
     console.log("Connected Successfully to server");
 
     const db = client.db(dbname);
-    client.close();
-})
+
+    insertDocuments( db, function() {
+        client.close();
+    })
+});
+
+const insertDocuments = ( db, callback) => {
+    const collection = db.collection( 'fruits');
+
+    collection.insertMany([
+        {
+            name: "Apple",
+            score: 8
+        },
+        {
+            name: "Orange",
+            score: 6
+        },
+        {
+            name: "Banana",
+            score: 9
+        }
+    ], ( err, result) => {
+        assert.equal(err, null);
+        console.log("Inserted 3 documents into the collections");
+        callback(result);
+    });
+};
